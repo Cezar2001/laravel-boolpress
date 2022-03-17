27dd1525cbfce0a00fd030e01bfe1d8c9f3e1971
Cezar <cezarlantosgmail.com>
2022-03-17 10:57:36 +0100

    add font awesome

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -8,14 +8,15 @@
                     <div class="card-header d-flex">
                         Lista dei post
 
-                        <a class="ms-auto" href="{{ route('admin.posts.create') }}">Aggiungi...</a>
+                        <a class="ms-auto" href="{{ route('admin.posts.create') }}"><i class="fa-solid fa-plus"></i>Aggiungi</a>
                     </div>
 
                     <div class="card-body">
                         <ul class="list-group">
                             @foreach ($posts as $post)
                                 <li class="list-group-item">{{$post->title}}
-                                    <a href="{{ route('admin.posts.show', $post->slug) }}">Mostra</a>
+                                    <a class="ms-3 me-3" href="{{ route('admin.posts.show', $post->slug) }}" title="Visualizza"><i class="fa-solid fa-eye"></i></a>
+                                    <a href="{{route('admin.posts.edit', $post->id)}}" title="Modifica"><i class="fa-solid fa-pen"></i></a>
                                 </li>
                             @endforeach
                         </ul>
